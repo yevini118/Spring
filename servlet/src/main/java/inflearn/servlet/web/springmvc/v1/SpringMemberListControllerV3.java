69fbc390bd83c7338611f9327b19ddd663581207
yevini118 <79689350+yevini118@users.noreply.github.com>
2022-11-30 19:48:42 +0900

    Create springmvc

---
+++ servlet/src/main/java/inflearn/servlet/web/springmvc/v1/SpringMemberListControllerV3.java
@@ -0,0 +1,27 @@
+package inflearn.servlet.web.springmvc.v1;
+
+import inflearn.servlet.domain.member.Member;
+import inflearn.servlet.domain.member.MemberRepository;
+import inflearn.servlet.web.frontcontroller.ModelView;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class SpringMemberListControllerV3 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+
+        List<Member> members = memberRepository.findAll();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+
+        return mv;
+    }
+}
